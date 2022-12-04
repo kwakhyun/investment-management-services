3b17682622e26575783e23131eaeb2b030dd5841
kwakhyun <khyun2348@naver.com>
2022-12-04 20:44:01 +0900

    Feat: middleware

---
+++ src/middleware/index.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { COOKIE_TOKEN_KEY, TOKEN_EXPIRED } from '@vo/CookieTokenRepository';
+
+export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/signin')) {
+    if (request.cookies.get(COOKIE_TOKEN_KEY) && !(request.cookies.get(COOKIE_TOKEN_KEY) === TOKEN_EXPIRED)) {
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_CLIENT_URL}`);
+    }
+  }
+  if (
+    request.nextUrl.pathname === '/' ||
+    request.nextUrl.pathname.startsWith('/accounts') ||
+    request.nextUrl.pathname.startsWith('/users')
+  ) {
+    if (!request.cookies.get(COOKIE_TOKEN_KEY)) {
+      return NextResponse.redirect(`${process.env.NEXT_PUBLIC_CLIENT_URL}/signin`);
+    }
+  }
+}
