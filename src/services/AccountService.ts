330fcb925a87aa048a330c12bba3ec7fc8eb4fd9
kwakhyun <khyun2348@naver.com>
2022-12-04 20:46:29 +0900

    Feat: services

---
+++ src/services/AccountService.ts
@@ -0,0 +1,41 @@
+import axios from '@utils/getAxios';
+import { Account } from 'src/types/account';
+import getQueryString from '@utils/getQueryString';
+
+const AccountsService = {
+  accountsQueryOptions: {
+    staleTime: 3 * 60 * 1000,
+    keepPreviousData: true,
+  },
+
+  accountsQueryConverter(key: string) {
+    switch (key) {
+      case 'page':
+        return '_page';
+      case 'limit':
+        return '_limit';
+      case 'brokerId':
+        return 'broker_id';
+      case 'isActive':
+        return 'is_active';
+      case 'search':
+        return 'q';
+      default:
+        return key;
+    }
+  },
+
+  async getAccounts(params: Record<string, unknown>) {
+    const { data, headers } = await axios({ bearer: true }).get<Account[]>(
+      `/accounts?${getQueryString(params, this.accountsQueryConverter)}`
+    );
+    return { data, totalLength: Number(headers?.['x-total-count'] ?? 0) };
+  },
+
+  async patchAccount(id: string, data: { name: string }) {
+    const res = await axios({ bearer: true }).patch<Account>(`/accounts/${id}`, data);
+    return res.data;
+  },
+};
+
+export default AccountsService;
---
+++ src/services/UserService.ts
@@ -0,0 +1,51 @@
+import axios from '@utils/getAxios';
+import { User } from 'src/types/user';
+import getQueryString from '@utils/getQueryString';
+
+const UserService = {
+  usersQueryOptions: {
+    staleTime: 3 * 60 * 1000,
+    keepPreviousData: true,
+  },
+
+  usersQueryConverter(key: string) {
+    switch (key) {
+      case 'page':
+        return '_page';
+      case 'limit':
+        return '_limit';
+      case 'isActive':
+        return 'is_active';
+      case 'isStaff':
+        return 'is_staff';
+      case 'search':
+        return 'q';
+      default:
+        return key;
+    }
+  },
+
+  async getUsers(params: Record<string, unknown>) {
+    const { data, headers } = await axios({ bearer: true }).get<User[]>(
+      `/users?${getQueryString(params, this.usersQueryConverter)}`
+    );
+    return { data, totalLength: Number(headers?.['x-total-count'] ?? 0) };
+  },
+
+  async getUserById({ id }: { id: number }) {
+    const res = await axios({ bearer: true }).get<User[]>(`/users?id=${id}`);
+    return res.data[0];
+  },
+
+  async patchUser(id: string, data: { name: string }) {
+    const res = await axios({ bearer: true }).patch<User>(`/users/${id}`, data);
+    return res.data;
+  },
+
+  async deleteUser(id: string) {
+    const res = await axios({ bearer: true }).delete(`/users/${id}`);
+    return res.data;
+  },
+};
+
+export default UserService;
