a0fd6d2529a8753e69136e290e4b2a6d3c14e076
kwakhyun <khyun2348@naver.com>
2022-12-04 20:40:04 +0900

    Feat: signin components

---
+++ src/components/signin/alertModal.tsx
@@ -0,0 +1,60 @@
+import { useSelector, useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import { HiInformationCircle, HiOutlineX } from 'react-icons/hi';
+
+import { flexBox, positionCenterX } from '@styles/mixins';
+import { alertModalState, showModal, unshowModal } from '@store/modules/alertModalSlice';
+import { useEffect } from 'react';
+
+type Props = {
+  isExpired: boolean;
+};
+
+function alertModal({ isExpired }: Props) {
+  const { alertModal } = useSelector((state: { alertModal: alertModalState }) => state);
+  const dispatch = useDispatch();
+
+  const handleClick = () => dispatch(unshowModal());
+
+  useEffect(() => {
+    if (isExpired) dispatch(showModal('세션이 만료되어 재로그인이 필요합니다.'));
+  }, [isExpired]);
+
+  if (!alertModal.isOpen) return null;
+
+  return (
+    <Container>
+      <HiInformationCircle />
+      {alertModal.content}
+      <HiOutlineX onClick={handleClick} />
+    </Container>
+  );
+}
+
+export default alertModal;
+
+const Container = styled.div`
+  ${positionCenterX()}
+  top: -60px;
+  ${flexBox()}
+  width: 320px;
+  height: 40px;
+  background-color: ${({ theme }) => theme.YELLOW_BG};
+  border: 1px solid ${({ theme }) => theme.YELLOW};
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: 500;
+
+  > svg:first-child {
+    margin-right: 5px;
+    color: ${({ theme }) => theme.YELLOW};
+    font-size: 16px;
+  }
+
+  > svg:last-child {
+    margin-left: 10px;
+    color: ${({ theme }) => theme.GRAY_DARK};
+    font-size: 18px;
+    cursor: pointer;
+  }
+`;
---
+++ src/components/signin/index.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+import { HiOutlineUser, HiLogin } from 'react-icons/hi';
+
+import Button from '@components/signin/Button';
+import Input from '@components/signin/Input';
+import FormTitle from '@components/signin/FormTitle';
+import Seo from '@components/Layout/Seo';
+import useSignin from '@hooks/useSignin';
+import { flexBox } from '@styles/mixins';
+import ReLoginModal from './alertModal';
+
+type Props = {
+  isExpired: boolean;
+};
+
+function Signin({ isExpired }: Props) {
+  const { email, password, isFormValid, handleEmailChange, handlePasswordChange, handleSubmit } = useSignin();
+
+  return (
+    <Container>
+      <Seo title="D. PREFACE | 로그인" />
+      <Title>PREFACE</Title>
+      <SubContainer>
+        <ReLoginModal isExpired={isExpired} />
+        <FormTitle Icon={HiOutlineUser}>로그인</FormTitle>
+        <Form onSubmit={handleSubmit}>
+          <Input value={email} onChange={handleEmailChange} type="text" placeholder="아이디를 입력하세요" />
+          <Input value={password} onChange={handlePasswordChange} type="password" placeholder="비밀번호를 입력하세요" />
+          <Button type="submit" Icon={HiLogin} isValid={isFormValid}>
+            로그인
+          </Button>
+        </Form>
+      </SubContainer>
+    </Container>
+  );
+}
+
+export default Signin;
+
+const Container = styled.div`
+  ${flexBox('column')}
+  width: 100vw;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.GRAY_BG};
+`;
+
+const Title = styled.h1`
+  font-size: 40px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.NAVY};
+  margin-bottom: 80px;
+`;
+
+const SubContainer = styled.div`
+  position: relative;
+  width: 400px;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0 0 5px ${({ theme }) => theme.GRAY_DARK};
+`;
+
+const Form = styled.form`
+  ${flexBox('column', 'space-between')}
+  width: 100%;
+  height: 230px;
+  padding: 30px 20px;
+`;
