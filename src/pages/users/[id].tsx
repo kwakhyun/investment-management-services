ef379af26925a017cbbc255c3b2639170701cd8f
kwakhyun <khyun2348@naver.com>
2022-12-04 20:46:11 +0900

    Feat: users pages

---
+++ src/pages/users/[id].tsx
@@ -0,0 +1,47 @@
+import { GetServerSideProps } from 'next';
+import axios, { AxiosError } from 'axios';
+
+import { COOKIE_TOKEN_KEY, TOKEN_EXPIRED } from '@vo/CookieTokenRepository';
+import { User } from 'src/types/user';
+import { Account } from 'src/types/account';
+import UserDetailView from '@components/UserDetail';
+
+type Props = {
+  user: User;
+  accounts: Account[];
+};
+
+function UserDetail({ user, accounts }: Props) {
+  return <UserDetailView user={user} accounts={accounts} />;
+}
+
+export default UserDetail;
+
+export const getServerSideProps: GetServerSideProps = async ({ params, req, res }) => {
+  const token = req.cookies[COOKIE_TOKEN_KEY];
+  const id = params?.userId;
+  let userRes;
+  let accountsRes;
+
+  try {
+    userRes = await axios.get<User>(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/${id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    accountsRes = await axios.get<User>(`${process.env.NEXT_PUBLIC_SERVER_URL}/accounts?user_id=${userRes.data.uuid}`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  } catch (error) {
+    if (error instanceof AxiosError && error.response?.status === 401) {
+      res.setHeader('Set-Cookie', [`${COOKIE_TOKEN_KEY}=${TOKEN_EXPIRED}; Path=/`]);
+      return {
+        redirect: {
+          destination: '/signin',
+        },
+        props: {},
+      };
+    }
+  }
+
+  return { props: { user: userRes?.data, accounts: accountsRes?.data } };
+};
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,47 @@
+import { GetServerSideProps } from 'next';
+import axios, { AxiosError } from 'axios';
+
+import UsersView from '@components/Users';
+import { COOKIE_TOKEN_KEY, TOKEN_EXPIRED } from '@vo/CookieTokenRepository';
+import type { User } from 'src/types/user';
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+import getQueryString from '@utils/getQueryString';
+import UserService from '@services/UserService';
+
+function Users() {
+  return <UsersView />;
+}
+
+export default Users;
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res, query }) => {
+  const token = req.cookies[COOKIE_TOKEN_KEY];
+  const { page, limit, is_active: isActive = 'all', is_staff: isStaff = 'all', search = '' } = query;
+  const initialQuery = { page: Number(page) || 1, limit: Number(limit) || 30, isActive, isStaff, search };
+  const queryClient = new QueryClient();
+
+  try {
+    const { data, headers } = await axios.get<User>(
+      `${process.env.NEXT_PUBLIC_SERVER_URL}/users?${getQueryString(initialQuery, UserService.usersQueryConverter)}`,
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const queryFn = () => Promise.resolve({ data, totalLength: Number(headers?.['x-total-count'] ?? 0) });
+    await queryClient.prefetchQuery(['users', initialQuery], queryFn);
+  } catch (error) {
+    if (error instanceof AxiosError && error.response?.status === 401) {
+      res.setHeader('Set-Cookie', [`${COOKIE_TOKEN_KEY}=${TOKEN_EXPIRED}; Path=/`]);
+      return {
+        redirect: {
+          destination: '/signin',
+        },
+        props: {},
+      };
+    }
+  }
+
+  return {
+    props: { dehydratedState: dehydrate(queryClient) },
+  };
+};
